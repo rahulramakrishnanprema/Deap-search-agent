dd4a6d0461ce36a87835db98e7073ff02ab3ee8e
Rahulr <rahulramakrishnan274@gmail.com>
2025-08-26 22:16:13 +0530

    chore: update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,7 +1,6 @@
 def main():
  while True:
-  print("1. Add 2. Sub 3. Mul 4. Div 5. Exit")
-  choice = input()
-  if choice == '5': break
-  # ... (rest of the calculator logic)
+  op = input()
+  if op == 'exit': break
+  print(eval(op))
 main()
