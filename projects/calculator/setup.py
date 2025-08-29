28b255c2aa11a5976ab567ddb8833145c2928169
Rahulr <rahulramakrishnan274@gmail.com>
2025-08-29 17:42:59 +0530

    Add setup.py for calculator project

---
+++ projects/calculator/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="calculator",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies from requirements.txt
+    ],
+    author="AutoFlow Pro",
+    description="Calculator project",
+)
